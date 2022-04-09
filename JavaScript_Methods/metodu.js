bebf43b614761f4d453c2e2182939cc005160d15
Lera <Valerya12002@mail.ru>
2022-04-09 23:27:09 +0300

    10.04.22

--- JavaScript_Methods/metodu.js
+++ JavaScript_Methods/metodu.js
@@ -8,7 +8,9 @@ function closeToN(arr) {
 
 //1.2
 function numbersAfterN(arr) {
-    return [str.slice(arr.indexOf("n", 0), 9)]
+    var n = arr.indexOf("n", 0);
+    var ExitArr = arr.slice(n + 1, arr.length);
+    return ExitArr;
 }
 
 //1.3
@@ -16,7 +18,30 @@ function mergerNumbersAfterN(arr, arr1) {
     return (arr.splice(arr.indexOf("n", 0) + 1, arr.length - arr.indexOf("n", 0)).concat(arr1.slice(0, arr1.indexOf("n", 0))));
 }
 
+
+var arr = [1, 2, 3, 4, 5/*...*/];
+
 //2.2
 function reverseString(arr) {
     return arr.join('-', arr.reverse());
+}
+
+var str = "?Я?предпочитаю?вести?здоровый?образ?жизни";
+
+//3.1
+function splitStr(str) {
+    var ExitArr = str.split("?");
+    return ExitArr;
+}
+
+var str = "JavaScript";
+
+//Упражнение 4.2
+function startOrEnd(str, underStr) {
+    if (str.startsWith(underStr) == true || str.endsWith(underStr) == true) {
+        return true
+    } else {
+        return false
+    }
+    console.log(startOrEnd(str, "pt"));
 }
